50c795a2008034e0e6f99faa5d3ec4db99f388c6
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-09-29 03:48:35 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import '../styles/footer.css'
+import React from 'react';
+import '../styles/footer.css';
+import logo from '../assets/logo.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'><img src={logo} alt='Logo' /></a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Skills</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='footer__copyright'>
+        <small>&copy; CrisHSansaloni 2022</small>
+      </div>
+    </footer>
   )
 }
 
-export default Footer
+export default Footer;
