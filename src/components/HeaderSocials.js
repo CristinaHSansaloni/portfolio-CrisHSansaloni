0ab5bf492a6ac428b21c6f40259565bbc2cdab2a
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-10-08 04:51:12 +0200

    solved problems

--- src/components/HeaderSocials.js
+++ src/components/HeaderSocials.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {AiOutlineLinkedin} from 'react-icons/ai';
 import {AiOutlineGithub} from 'react-icons/ai';
 import {AiOutlineTwitter} from 'react-icons/ai';
+import {FaCodepen} from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
@@ -9,6 +10,7 @@ const HeaderSocials = () => {
       <a href='https://www.linkedin.com/in/crishsansaloni/'><AiOutlineLinkedin /></a>
       <a href='https://github.com/CristinaHSansaloni'><AiOutlineGithub /></a>
       <a href='https://twitter.com/CrisHSansaloni'><AiOutlineTwitter /></a>
+      <a href='https://codepen.io/cristinahsansaloni'><FaCodepen /></a>
     </div>
   )
 }
