e743efadf33cc58b64a48bf2f555ea965a9aa2c8
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-10-08 05:49:30 +0200

    responsive nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,18 +5,33 @@ import {FaUserAstronaut} from 'react-icons/fa';
 import {FaCode} from 'react-icons/fa';
 import {BsImages} from 'react-icons/bs';
 import {RiMessage3Fill} from 'react-icons/ri';
+import {FaBars} from 'react-icons/fa';
+import {FaTimes} from 'react-icons/fa';
 import { useState } from 'react';
+import { useRef } from 'react';
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState('#');
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
   return (
-    <nav>
-      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
-      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut /></a>
-      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaCode /></a>
-      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsImages /></a>
-      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage3Fill /></a>
-    </nav>
+    <header>
+      <nav ref={navRef}>
+        <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiFillHome /></a>
+        <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut /></a>
+        <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaCode /></a>
+        <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BsImages /></a>
+        <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage3Fill /></a>
+        <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className='nav-btn' onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   )
 }
 
